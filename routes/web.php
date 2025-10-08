56bf67cf36a29db59853c30fe9dd1ae1f0dbd93c
Muhammad Khoirul Anam <mkhoirulanam920@gmail.com>
2025-10-08 14:28:19 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// Route::get('admin', function () {
+//     return 'Hi Admin';
+// })->middleware('role:admin');
+
+// Route::get('user', function () {
+//     return 'Hi user';
+// })->middleware('role:user');
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::get('/', function () {
+    return Inertia('Login');
+})->name('login');
+
+
+require __DIR__.'/auth.php';
